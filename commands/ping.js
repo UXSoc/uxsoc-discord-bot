f105372b012469506abead50d46d2e9a3ef13e51
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-24 23:32:55 +0800

    Revert "modulize commands"
    This reverts commit 577ce819deefd1c68f840298d7926031bf34e73a.

--- commands/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (message, args, accountsdata) => {
-    const msg = await message.channel.send('Pinging...');
-    const latency = msg.createdTimestamp - message.createdTimestamp;
-    const apiLatency = Math.round(message.client.ws.ping);
-    await msg.edit(`Pong! 🏓\nLatency: \`${latency}ms\`. API Latency: \`${apiLatency}ms\``);
-};
