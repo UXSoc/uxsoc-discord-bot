c85ffc9228d6b707a01c6495423f99ee6f339d4e
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-25 18:21:42 +0800

    make reading accounts more reliable

--- DiscordClient.js
+++ DiscordClient.js
@@ -26,33 +26,28 @@ const dev_channels = config.channels.dev;
 const hype_channels = config.channels.hype;
 
 var accountsdata;
-function loadAccounts() {
-    if (!fs.existsSync(accounts)) {
-        fs.writeFile(accounts, JSON.stringify({}), (error) => {
-            if (error) {
-                throw new Error('Failed to create accounts.json: ' + error.message);
-            }
-            console.log('[INFO] Initialized accounts.json');
-            loadAccounts();
-        });
-    } else {
-        fs.readFile(accounts, 'utf8', (error, data) => {
-            if (error) {
-                throw new Error('Error reading accounts.json: ' + error.message);
-            }
-            try {
-                accountsdata = JSON.parse(data);
-            } catch (parseError) {
-                throw new Error('Error parsing JSON: ' + parseError.message);
-            }
-        });
+if (!fs.existsSync(accounts)) {
+    try {
+        fs.writeFileSync(accounts, JSON.stringify({}))
+    } catch (error) {
+        throw new Error('Failed to create accounts.json: ' + error.message);
     }
+    console.log('[INFO] Initialized accounts.json');
 }
-loadAccounts();
+try {
+    const data = fs.readFileSync(accounts, 'utf8');
+    try {
+        accountsdata = JSON.parse(data);
+    } catch (parseError) {
+        throw new Error('Error parsing JSON: ' + parseError.message);
+    }
+} catch (error) {
+    throw new Error('Error reading accounts.json: ' + error.message);
+}
 
 client.login(process.env.DISCORD_TOKEN);
 client.on('ready', () => {
-    if (ENVIRONMENT == "DEV") console.log("Started in Developer Mode")
+    if (ENVIRONMENT == "DEV") console.log("[INFO] Started in Developer Mode")
     console.log('UXBot connected and ready!');
     client.user.setActivity(config.activity.name, { type: config.activity.type })
 })
