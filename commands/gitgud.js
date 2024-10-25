ee61364496c90da992fed8f7a18ed0c3fa12b83b
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-25 16:13:36 +0800

    fix error when nickname is null

--- commands/gitgud.js
+++ commands/gitgud.js
@@ -24,7 +24,8 @@ module.exports = async (message, args, accountsdata) => {
         if (baseBranch !== 'staging') return message.reply(`Invalid pull request. This pull request wants to merge into __${baseBranch}__ instead of __staging__.`);
         if  (username !== accData.github) return message.reply(`Pull request author mismatch. Owned by __${username}__ instead of __${accData.github}__.`);
         await message.member.roles.add("1294311955401674762");
-        message.reply(`Congratulations **${message.member.nickname.split(" ")[0]}**! You're now git good 🎉! You can now contribute to community projects!`)
+        const name = (message.member.nickname)?message.member.nickname.split(" ")[0]:username
+        message.reply(`Congratulations **${name}**! You're now git good 🎉! You can now contribute to community projects!`)
     } catch (error) {
         return message.reply(`An error occurred. ${error}`)
     }
