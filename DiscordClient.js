8dbd26a109b10ce03f5c8b820384cd234bfe6731
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-25 16:32:20 +0800

    add dm intent

--- DiscordClient.js
+++ DiscordClient.js
@@ -11,7 +11,8 @@ const client = new Client({ intents: [
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.MESSAGE_CONTENT,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES
     ],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
