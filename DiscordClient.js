cd313d86fc4b877f9f563b3b9a0ab26fcbd6a30c
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-25 16:35:12 +0800

    fix unable to dm users

--- DiscordClient.js
+++ DiscordClient.js
@@ -102,14 +102,14 @@ client.on('messageReactionAdd', async (reaction, user) => {
                 const dscrd_nick = message.member.nickname;
                 if (!dscrd_nick || (dscrd_nick && (dscrd_nick.split(" ").length < 2))) {
                     removeReaction(user, message, reactionEmoji);
-                    return message.author.send(`Hey ${user.username} 👋, please follow the server profile name format as seen in <#${config.channels.rules}> before joining a project.`).catch(error => {
+                    return user.send(`Hey ${user.username} 👋, please follow the server profile name format as seen in <#${config.channels.rules}> before joining a project.`).catch(error => {
                         console.log("could not send dm to user.")
                     });
                 }
                 await member.roles.add(config.allow_project_join_role);
             } else {
                 removeReaction(user, message, reactionEmoji);
-                return message.author.send(`Hey ${user.username} 👋, please complete the **Git Gud at Git Workshop** to be able to contribute to community projects.`).catch(error => {
+                return user.send(`Hey ${user.username} 👋, please complete the **Git Gud at Git Workshop** to be able to contribute to community projects.`).catch(error => {
                     console.log("could not send dm to user.")
                 });
             }
