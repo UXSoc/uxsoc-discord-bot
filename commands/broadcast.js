099d96fc7c71dfb8728d0059e4918d421d6a2ee7
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-28 17:20:00 +0800

    add officer role check

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -1,3 +1,4 @@
+const config = require('../config.json');
 module.exports = async (message, args, accountsdata, client) => {
     let repliedTo;
     if (message.reference?.messageId) {
@@ -10,7 +11,9 @@ module.exports = async (message, args, accountsdata, client) => {
         return message.reply('Error: No message given.');
     }
     const targetChannelId = args[1].substring(2,21);
-    if (!repliedTo) {message.reply('Error: No message given.');}
-    if (!targetChannelId) {message.reply('Error: No target channel given.');}
+    if (!repliedTo) return message.reply('Error: No message given.');
+    const member = await message.guild.members.fetch(repliedTo.author.id);
+    if (!member.roles.cache.has(config.officer_roleId)) return message.reply('You do not have permission to run this command.');
+    if (!targetChannelId) return message.reply('Error: No target channel given.');
     client.channels.cache.get(targetChannelId).send(repliedTo.content);
 }
