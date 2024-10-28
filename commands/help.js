3a7cbe0e674875a518a0cda389a9dad31a9c7b9d
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-10-28 17:44:52 +0800

    add broadcast to help menu

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 const embed = require('../utils/embed');
 
-const commands = [
+const global_commands = [
     {
         name: "help",
         args: [],
@@ -23,19 +23,30 @@ const commands = [
         desc: "Verify that you\'ve completed the Git Gud workshop with your pull request to gain access to Community Projects"
     }
 ]
-var command_fields = []
-for (let i = 0; i < commands.length; i++) {
-    var value = `__Usage:__ \`${config.prefix}${commands[i].name}`;
-    for (let j = 0; j < commands[i].args.length; j++) {
-        value += ` <${commands[i].args[j]}>`
+const admin_commands = [
+    {
+        name: "broadcast",
+        args: ["channel"],
+        desc: "Broadcast to a channel by replying to the message."
     }
-    value += '`\n'
-    command_fields.push({
-        name: `${config.prefix}${commands[i].name}`,
-        value: value + commands[i].desc,
-        inline: false
-    })
+]
+function createHelpFields(commands) {
+    var command_fields = [];
+    for (let i = 0; i < commands.length; i++) {
+        var value = `__Usage:__ \`${config.prefix}${commands[i].name}`;
+        for (let j = 0; j < commands[i].args.length; j++) {
+            value += ` <${commands[i].args[j]}>`
+        }
+        value += '`\n'
+        command_fields.push({
+            name: `${config.prefix}${commands[i].name}`,
+            value: value + commands[i].desc,
+            inline: false
+        })
+    }
+    return command_fields;
 }
 module.exports = async (message, args, accountsdata, client) => {
-    message.channel.send(await embed("🤖 UXBot Commands", "", command_fields))
+    const member = await message.guild.members.fetch(message.author.id);
+    message.channel.send(await embed("🤖 UXBot Commands", "", (member.roles.cache.has(config.officer_roleId))?createHelpFields(global_commands.concat(admin_commands)):createHelpFields(global_commands)))
 };
